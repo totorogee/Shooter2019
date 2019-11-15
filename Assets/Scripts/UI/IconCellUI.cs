020367766063b99df95f4281038bd3b23eb52b3b
Sum Yeung <totorogee@gmail.com>
2019-11-15 22:27:02 +0800

    Added button to choose group size

---
+++ Assets/Scripts/UI/IconCellUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconCellUI : CellUI
+{
+    public Text Text;
+    public Button Button;
+    public Image Image;
+    public Image Selected;
+
+    private void OnEnable()
+    {
+        Button.onClick.AddListener(OnPressed);
+    }
+
+    private void OnDisable()
+    {
+        Button.onClick.RemoveListener(OnPressed);
+    }
+
+
+    public void OnPressed()
+    {
+        ((IconCellUIController)MyController).OnCellPressed(this);
+    }
+}
+
+
---
+++ Assets/Scripts/UI/IconCellUIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconCellUIController : CellUIController<IconCellUI>
+{
+    public void Select(int ID)
+    {
+        foreach (var item in CellsList)
+        {
+            item.Selected.gameObject.SetActive(false);
+        }
+
+        CellsList[ID].Selected.gameObject.SetActive(true);
+        GroupSettingSceneUIController.Instance.CurrentGroupIconID = ID;
+
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Select(GroupSettingSceneUIController.Instance.CurrentGroupIconID);
+
+    }
+
+    public void OnCellPressed(IconCellUI cellUI)
+    {
+        for (int i = 0; i < CellsList.Count; i++)
+        {
+            if (CellsList[i] == cellUI)
+            {
+                Select(i);
+                return;
+            }
+        }
+    }
+
+}
