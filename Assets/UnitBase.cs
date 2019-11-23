72aae924fd34a7d29e0bedbf88d3faa519b4bb0b
Sum Yeung <totorogee@gmail.com>
2019-11-23 14:02:50 +0800

    Setup groups and units in fleet

---
+++ Assets/UnitBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitBaseState
+{
+    Nil =0,
+    Stunned =1,
+    Dead =2
+}
+
+public class UnitBase : MonoBehaviour
+{
+    public static Color DamageColor = Color.blue;
+    public static Color NormalColor = Color.red;
+    public Transform Body;
+
+
+    public float HP = 100;
+    public float HPMax = 100;
+    public float Recover = 20;
+
+    public UnitBaseState UnitState = UnitBaseState.Nil;
+
+    public void Update()
+    {
+        if (HP < HPMax && HP > 0)
+        {
+            HP += Recover * Time.deltaTime;
+        }
+
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        var material = Body.GetComponent<MeshRenderer>().material;
+        material.color = Color.Lerp(DamageColor, NormalColor, (float)HP / (float)HPMax);
+
+        if(HP <= 0 && UnitState == UnitBaseState.Nil)
+        {
+            UnitState = UnitBaseState.Dead;
+            transform.localPosition = transform.localPosition * 2f;
+            Body.transform.localEulerAngles = new Vector3(0f ,0f, Random.Range(0f, 180f));
+        }
+    }
+
+    private void OnEnable()
+    {
+        HP = HPMax/5;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        HP -= damage;
+    }
+}
---
+++ Assets/UnitGroup.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeamName
+{
+    Red =0,
+    Blue =1
+}
+
+public class UnitGroup : MonoBehaviour
+{
+    public List<UnitBase> Bases = new List<UnitBase>();
+
+    public static List<UnitGroup> AllRed = new List<UnitGroup>();
+    public static List<UnitGroup> AllBlue = new List<UnitGroup>();
+
+    public LineRenderer AttackLine;
+
+
+    public TeamName MyTeam = TeamName.Red;
+
+
+    public float GroupDamagePool = 2;
+    public float GroupShareMinHP = 10;
+
+
+    private float AttackRange = 20f;
+    private float AttackDamage = 10f;
+
+    private bool didInit;
+
+    private void OnEnable()
+    {
+        AttackLine.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (!didInit)
+        {
+            Init();
+        }
+
+        if (didInit)
+        {
+            if (Time.frameCount % 40 == 0)
+            {
+                CheckAttack();
+            }
+        }
+
+
+    }
+
+    private void Init()
+    {
+        didInit = true;
+
+        if(MyTeam == TeamName.Red)
+        {
+            AllRed.Add(this);
+        }
+        else
+        {
+            AllBlue.Add(this);
+        }
+
+        Bases = new List<UnitBase>(GetComponentsInChildren<UnitBase>());
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (Bases.Count == 0)
+        {
+            Debug.Log("Error");
+        }
+
+        foreach (var item in Bases)
+        {
+            item.TakeDamage(damage / Bases.Count);
+        }
+    }
+
+    public void CheckAttack()
+    {
+
+        if (MyTeam == TeamName.Red)
+        {
+            foreach (var item in AllBlue)
+            {
+                if ((transform.position - item.transform.position).magnitude < AttackRange)
+                {
+                    DisplayLine(AttackLine, item.transform.position, 0.2f);
+                    item.TakeDamage(AttackDamage);
+                }
+            }
+        }
+
+    }
+
+    public void DisplayLine(LineRenderer line , Vector3 target , float time)
+    {
+        line.enabled = true;
+        line.SetPosition(0,this.transform.position);
+        line.SetPosition(1,target);
+        StartCoroutine(DelayKillLine(line, time));
+    }
+
+    private IEnumerator DelayKillLine (LineRenderer line , float time)
+    {
+        line.enabled = true;
+        yield return new WaitForSeconds(time);
+        line.enabled = false;
+
+    }
+
+}
