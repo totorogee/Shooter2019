867a5a977adf9e9bcc948ef0267e28628dba5528
Sum Yeung <totorogee@gmail.com>
2019-11-23 18:15:48 +0800

    Removed Sandbox code; Relocated files

---
+++ Assets/Scripts/Scene/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tile
+{
+    public GameObject TileObject;
+    public int CircularID = 0;
+    public int ID = 0;
+    public PosVector Position = new PosVector();
+    public float Distance = 0;
+    public float Angle = 0;
+
+    public bool IsMainTile = false;
+    
+    // nearest tiles
+
+
+}
+
--- Assets/Scripts/Units/Fleet.cs
+++ Assets/Scripts/Units/Fleet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitsController : MonoBehaviour
+public class Fleet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
