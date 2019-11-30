64278745fc7c7e496b428114407520a31064ac05
Sum Yeung <totorogee@gmail.com>
2019-11-30 14:14:59 +0800

    Added EnumUtil for get enum values

---
+++ Assets/Scripts/Tools/EnumUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public static class EnumUtil
+{
+    public static IEnumerable<T> GetValues<T>()
+    {
+        return Enum.GetValues(typeof(T)).Cast<T>();
+    }
+
+    public static List<T> GetValuesList<T>()
+    {
+        List<T> Result = new List<T>(Enum.GetValues(typeof(T)).Cast<T>());
+        return Result;
+    }
+
+}
